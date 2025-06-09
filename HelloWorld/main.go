fc8e5f683098f2434f7d9a4fa5137961cd094936
pietro <pietroluigicarotenuto@gmail.com>
2025-06-09 09:09:02 +0200

    Updated HelloWorld example

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
 	fmt.Println("This is a simple Go program.")
+	fmt.Println(quote.Go())
+	fmt.Println("Let's build something amazing with Go!")
 }
