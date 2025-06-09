6e390b75599a4fbb5714a96ed73a7499a128a3f0
pietro <pietroluigicarotenuto@gmail.com>
2025-06-09 11:16:45 +0200

    Added function to return random greeting sentence

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -16,4 +17,20 @@ func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+}
+
+func RandomGreeting() (string, error) {
+	// List of greetings
+	greetings := []string{
+		"Hello, World!",
+		"Welcome to Go programming!",
+		"Hi there, Gopher!",
+		"Greetings from the Go team!",
+	}
+
+	// Generate a random index
+	index := rand.Intn(len(greetings))
+
+	// Return a random greeting
+	return greetings[index], nil
 }
